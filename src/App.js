bc2364563ea6c2fbf22d728d21fcdeaa9e4b9b52
mhkasif <haseebasif97@gmail.com>
2020-07-31 18:35:36 +0500

    comment removed

--- src/App.js
+++ src/App.js
@@ -12,12 +12,7 @@ class BooksApp extends React.Component {
     currentlyReading: [],
     wantToRead: [],
     read: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+
   };
   componentDidMount = async () => {
     const books = await BooksAPI.getAll();
